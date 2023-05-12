dfcd438eeeadd8cf233a7ef5a231575bec45663c
Aleksandr <shtilnuhcenberg@gmail.com>
2023-05-12 13:13:41 +0300

    edit files to complete routing tests

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./../dist/'));
+app.use(express.static('./dist/'));
 
-app.get('./*/', (req, res) => {
-  return res.sendFile('./src/index.hbs');
-});
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
