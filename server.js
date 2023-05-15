257b7169ec1572ccca8d5a6393deed70c70306cd
Aleksandr <shtilnuhcenberg@gmail.com>
2023-05-15 15:31:57 +0300

    make corrections

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./../dist/'));
+app.use(express.static('./dist/'));
+
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
